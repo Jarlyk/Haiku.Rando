3f437b61b7c835d0bc2f5479a415e9be332a4e90
Diogo Pinela <diogoid7400@gmail.com>
2023-07-27 02:27:09 +0100

    Allow unlocked doors to open again

--- Haiku.Rando/Checks/LeverReplacer.cs
+++ Haiku.Rando/Checks/LeverReplacer.cs
@@ -22,23 +22,20 @@ namespace Haiku.Rando.Checks
 
         private static void ChangeActivationCondition(On.SwitchDoor.orig_Start orig, SwitchDoor self)
         {
+            // Let the door open if it has been unlocked
+            // whether by a vanilla lever, or by a corresponding rando
+            // lever pickup.
+            orig(self);
+            
             var lr = self.GetComponent<LeverReplacer>();
-            if (lr == null)
+            if (lr != null)
             {
-                orig(self);
-                return;
-            }
-            self.switchAnim = self.GetComponent<Animator>();
-            self.switchCollider = self.GetComponent<Collider2D>();
-            if (CheckManager.AlreadyGotCheck(lr.replacement))
-            {
-                self.switchAnim.SetBool("open", true);
-                self.switchCollider.enabled = false;
+                var gotCheck = CheckManager.AlreadyGotCheck(lr.replacement);
+                self.switchAnim.SetBool("open", gotCheck);
+                self.switchCollider.enabled = !gotCheck;
             }
         }
 
-        // The last parameter can't be a Vector2; somehow the MAPI hook
-        // is defined as taking an `object` instead of Vector2.
         private static void ReplaceItem(Action<SwitchDoor, int, int, Vector2> orig, SwitchDoor self, int a, int b, Vector2 playerPos)
         {
             var lr = self.GetComponent<LeverReplacer>();
