aa051467a7ae33a467de502bad1739984d44f1be
Diogo Pinela <diogoid7400@gmail.com>
2023-08-29 23:48:54 +0100

    Increment version

--- Haiku.Rando/RandoPlugin.cs
+++ Haiku.Rando/RandoPlugin.cs
@@ -20,7 +20,7 @@ using Object = UnityEngine.Object;
 
 namespace Haiku.Rando
 {
-    [BepInPlugin("haiku.rando", "Haiku Rando", "2.2.1.0")]
+    [BepInPlugin("haiku.rando", "Haiku Rando", "2.2.2.0")]
     [BepInDependency("haiku.mapi", "1.0")]
     public sealed class RandoPlugin : BaseUnityPlugin
     {
